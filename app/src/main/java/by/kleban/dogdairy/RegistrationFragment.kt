b7dcc040ff694c094ab9fb7fa557c7a6078a43c3
Ella Kleban <ella.kleban@gmail.com>
2021-05-26 18:52:40 +0300

    create button in fragment_registration.xml

--- app/src/main/java/by/kleban/dogdairy/RegistrationFragment.kt
+++ app/src/main/java/by/kleban/dogdairy/RegistrationFragment.kt
@@ -4,30 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.textfield.TextInputLayout
 
 
-class RegistrationFragment: Fragment() {
+class RegistrationFragment : Fragment() {
 
-    private lateinit var inputLayoutName:TextInputLayout
-    private lateinit var inputLayoutBreed:TextInputLayout
+    private lateinit var inputLayoutName: TextInputLayout
+    private lateinit var inputLayoutBreed: TextInputLayout
+    private lateinit var breedButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_registration,container,false)
+        return inflater.inflate(R.layout.fragment_registration, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        inputLayoutName=view.findViewById(R.id.txt_input_registration_name)
-        inputLayoutBreed=view.findViewById(R.id.txt_input_registration_breed)
-        inputLayoutBreed.editText?.setOnClickListener {
+        inputLayoutName = view.findViewById(R.id.txt_input_registration_name)
+        inputLayoutBreed = view.findViewById(R.id.txt_input_registration_breed)
+        breedButton = view.findViewById(R.id.btn_choose_breed)
+
+        breedButton.setOnClickListener {
             findNavController().navigate(R.id.showShowBreedsFragment)
         }
     }
