0f9212c27b578631bb1b1daa26b79e4a380ef91d
Ella Kleban <ella.kleban@gmail.com>
2021-05-26 14:22:41 +0300

    create RegistrationFragment

--- app/src/main/java/by/kleban/dogdairy/SplashScreenFragment.kt
+++ app/src/main/java/by/kleban/dogdairy/SplashScreenFragment.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.*
 
 
 class SplashScreenFragment : Fragment() {
+
+    private val ioScope = CoroutineScope(Dispatchers.IO)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +19,15 @@ class SplashScreenFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        ioScope.launch {
+            delay(4000)
+            withContext(Dispatchers.Main) {
+                findNavController().navigate(R.id.showRegistrationFragment) }
+        }
     }
 }
