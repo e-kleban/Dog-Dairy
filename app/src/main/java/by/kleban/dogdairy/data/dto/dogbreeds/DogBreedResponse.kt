86364674e74b4b057a9a20b27d8b436fc82f9cd2
Ella Kleban <ella.kleban@gmail.com>
2021-05-30 19:29:09 +0300

    add fields in DogBreedResponse.kt, DogBreed.kt. correct DogBreedMapper.kt

--- app/src/main/java/by/kleban/dogdairy/data/dto/dogbreeds/DogBreedResponse.kt
+++ app/src/main/java/by/kleban/dogdairy/data/dto/dogbreeds/DogBreedResponse.kt
@@ -10,9 +10,25 @@ data class DogBreedResponse(
     val image: Image,
     @SerializedName("name")
     val breed: String,
+    @SerializedName("bred_for")
+    val bredFor: String?,
+    @SerializedName("height")
+    val height: Height?,
+    @SerializedName("weight")
+    val weight: Weight?
 ) {
     data class Image(
         @SerializedName("url")
         val url: String,
+    )
+
+    data class Height(
+        @SerializedName("metric")
+        val centimeter: String?
+    )
+
+    data class Weight(
+        @SerializedName("metric")
+        val kilogram: String?
     )
 }
