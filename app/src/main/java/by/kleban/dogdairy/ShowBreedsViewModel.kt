eb5327677b1058f94e3ec83003abe7b622d6717c
Ella Kleban <ella.kleban@gmail.com>
2021-05-31 14:32:52 +0300

    strings in resources

--- app/src/main/java/by/kleban/dogdairy/ShowBreedsViewModel.kt
+++ app/src/main/java/by/kleban/dogdairy/ShowBreedsViewModel.kt
@@ -38,7 +38,7 @@ class ShowBreedsViewModel : ViewModel() {
             } catch (e: Exception) {
                 Log.e(ShowBreedsViewModel::class.java.simpleName, e.message.toString())
                 _isLoadingLiveData.postValue(false)
-                Toast.makeText(context, context.getString(R.string.problem_toast), Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, R.string.problem_toast, Toast.LENGTH_SHORT).show()
             }
         }
     }
