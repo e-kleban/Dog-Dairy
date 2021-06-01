ebbc42a802a575fa88a89f5fc9015f0526544ce2
Ella Kleban <ella.kleban@gmail.com>
2021-06-01 14:13:56 +0300

    create DogMapper, add functions in DogDao.kt, encapsulate code in private functions in RegistrationFragment.kt

--- app/src/main/java/by/kleban/dogdairy/database/DogRoomDb.kt
+++ app/src/main/java/by/kleban/dogdairy/database/DogRoomDb.kt
@@ -8,11 +8,17 @@ import by.kleban.dogdairy.entities.Dog
 
 class DogRoomDb(
     private val dogDao: DogDao,
-    private val dbDogMapper: Mapper<DbDog, Dog>
+    private val dbDogMapper: Mapper<DbDog, Dog>,
+    private val dogMapper: Mapper<Dog, DbDog>
 ) : DogDb {
 
     override suspend fun getAllDog(): List<Dog> {
         return dogDao.getAllDog()
             .map { dbDogMapper.map(it) }
+    }
+
+    override suspend fun saveDog(dog: Dog) {
+        val dogDb = dogMapper.map(dog)
+        dogDao.saveDog(dogDb)
     }
 }
--- app/src/main/java/by/kleban/dogdairy/database/dao/DogDao.kt
+++ app/src/main/java/by/kleban/dogdairy/database/dao/DogDao.kt
@@ -1,5 +1,6 @@
 package by.kleban.dogdairy.database.dao
 
+import androidx.room.Insert
 import androidx.room.Query
 import by.kleban.dogdairy.database.entities.DbDog
 
@@ -7,5 +8,8 @@ import by.kleban.dogdairy.database.entities.DbDog
 interface DogDao {
 
     @Query("SELECT * FROM dog_table")
-    suspend fun getAllDog():List<DbDog>
+    suspend fun getAllDog(): List<DbDog>
+
+    @Insert
+    suspend fun saveDog(dogDb: DbDog)
 }
