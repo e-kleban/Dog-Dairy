105098f25563433e09ff6cf86a163c888120e107
Ella Kleban <ella.kleban@gmail.com>
2021-06-02 17:49:15 +0300

    add toast

--- app/src/main/java/by/kleban/dogdairy/ui/SplashScreenFragment.kt
+++ app/src/main/java/by/kleban/dogdairy/ui/SplashScreenFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -28,7 +29,7 @@ class SplashScreenFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.nextFragmentLiveData.observe(viewLifecycleOwner) { it ->
+        viewModel.nextFragmentLiveData.observe(viewLifecycleOwner) {
             if (it != null) {
                 when (it) {
                     Screen.REGISTRATION -> {
@@ -37,6 +38,10 @@ class SplashScreenFragment : Fragment() {
                     Screen.DOG -> findNavController().navigate(R.id.from_splashScreenFragment_to_dogPageFragment)
                 }
             }
+        }
+
+        viewModel.errorLiveData.observe(viewLifecycleOwner) {
+            Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
         }
     }
 }
